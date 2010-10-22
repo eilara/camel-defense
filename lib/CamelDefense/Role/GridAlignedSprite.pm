7c668272344270ed7580fd04637b6a3d489ae54f
eilara <ran.eilam@gmail.com>
2010-10-22 15:39:42 +0200

    start refactoring into roles

---
+++ lib/CamelDefense/Role/GridAlignedSprite.pm
@@ -0,0 +1,41 @@
+package CamelDefense::Role::GridAlignedSprite;
+
+use Moose::Role;
+use MooseX::Types::Moose qw(Num);
+use aliased 'SDLx::Sprite';
+
+requires 'compute_cell_center';
+
+has [qw(x y)] => (is => 'rw', required => 1, isa => Num);
+
+has sprite => (
+    is         => 'ro',
+    lazy_build => 1,
+    isa        => Sprite, 
+    handles    => [qw(load draw w h)],
+);
+
+has [qw(center_x center_y)] => (is => 'rw');
+
+sub BUILD {
+    my $self = shift;
+    $self->center_x($self->sprite->x + $self->w/2);
+    $self->center_y($self->sprite->y + $self->h/2);
+}
+
+sub _build_sprite {
+    my $self = shift;
+    my $center = $self->compute_cell_center($self->x, $self->y);
+    my $sprite = Sprite->new(image => '../data/tower.png');
+    $sprite->x($center->[0] - $sprite->w/2);
+    $sprite->y($center->[1] - $sprite->h/2);
+    return $sprite;
+}
+
+sub render {
+    my ($self, $surface) = @_;
+    $self->draw($surface);
+}
+
+1;
+
--- lib/CamelDefense/Tower.pm
+++ lib/CamelDefense/Tower.pm
@@ -7,15 +7,6 @@ use aliased 'SDLx::Sprite';
 use aliased 'CamelDefense::World';
 use aliased 'CamelDefense::Creep';
 
-has [qw(x y)] => (is => 'rw', required => 1, isa => Num);
-
-has sprite => (
-    is         => 'ro',
-    lazy_build => 1,
-    isa        => Sprite, 
-    handles    => [qw(load draw w h)],
-);
-
 has world => (
     is       => 'ro',
     required => 1,
@@ -32,22 +23,9 @@ has range           => (is => 'ro', isa => Num, default => 100); # in pixels
 has last_fire_time  => (is => 'rw', isa => Num, default => 0);
 has state           => (is => 'rw', isa => Str, default => 'init');
 
-has [qw(current_target last_damage_update center_x center_y)] => (is => 'rw');
+has [qw(current_target last_damage_update)] => (is => 'rw');
 
-sub BUILD {
-    my $self = shift;
-    $self->center_x($self->sprite->x + $self->w/2);
-    $self->center_y($self->sprite->y + $self->h/2);
-}
-
-sub _build_sprite {
-    my $self = shift;
-    my $center = $self->compute_cell_center($self->x, $self->y);
-    my $sprite = Sprite->new(image => '../data/tower.png');
-    $sprite->x($center->[0] - $sprite->w/2);
-    $sprite->y($center->[1] - $sprite->h/2);
-    return $sprite;
-}
+with 'CamelDefense::Role::GridAlignedSprite';
 
 sub move {
     my ($self, $dt)     = @_;
@@ -111,9 +89,8 @@ sub render_range {
     );
 }
 
-sub render {
+after render => sub {
     my ($self, $surface) = @_;
-    $self->draw($surface);
     # render laser to creep
     if ($self->state eq 'firing') {
         my $target = $self->current_target;
@@ -128,7 +105,7 @@ sub render {
             $self->current_target(undef);
         }
     }
-}
+};
 
 1;
 

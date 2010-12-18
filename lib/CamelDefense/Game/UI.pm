d2e5e5339caeedf78b9539f8af7ab5b39513c013
eilara <ran.eilam@gmail.com>
2010-12-18 10:08:49 +0200

    added button press down, still buggy

--- lib/CamelDefense/Game/UI.pm
+++ lib/CamelDefense/Game/UI.pm
@@ -6,15 +6,20 @@ use CamelDefense::Util qw(is_my_event);
 use aliased 'CamelDefense::Cursor';
 use aliased 'CamelDefense::Game::UI::Button';
 
-my $TOOLBAR_Y = 481;
+my $BTN_Y      = 482;
+my $BTN_NEXT_X = 597;
 
 has cursor   => (is => 'ro', required   => 1, isa => Cursor);
 has handler  => (is => 'ro', required   => 1, weak_ref => 1);
 has btn_next => (is => 'ro', lazy_build => 1, isa => Button);
 
-sub _build_btn_next { Button->new(
-    icon => '../data/ui_button_next.png', # 41x48
-)}
+sub _build_btn_next {
+    my $self = shift;
+    return Button->new(
+        click => sub { $self->handler->start_wave },
+        icon  => '../data/ui_button_next.png', # 43x44
+    );
+}
 
 with 'CamelDefense::Role::Sprite';
 
@@ -22,7 +27,7 @@ sub init_image_def { '../data/ui_toolbar.png' } # 640x48
 
 sub BUILD {
     my $self = shift;
-    $self->btn_next->xy([598, $TOOLBAR_Y]);
+    $self->btn_next->xy([$BTN_NEXT_X, $BTN_Y]);
 }
 
 sub handle_event {
@@ -31,17 +36,25 @@ sub handle_event {
 
     $self->cursor->set_default; # dont want build cursor on toolbar
     # route the event to the correct button
-    if ($e->type == SDL_MOUSEMOTION) {
-    } elsif ($e->type == SDL_MOUSEBUTTONDOWN) {
-    } elsif ($e->type == SDL_MOUSEBUTTONUP) {
+    my ($x, $y) = @{ $self->cursor->xy }; 
+    return unless $y >= $BTN_Y;
 
-        my ($x, $y) = @{ $self->cursor->xy }; 
-        return unless $y >= $TOOLBAR_Y + 1;
+    $self->forward_event($e, $x);
+}
 
-        if ($x >= 596) {
-            $self->handler->start_wave;
-        }
-    }
+sub forward_event {
+    my ($self, $e, $x) = @_;
+    my $type = $e->type;
+
+    my $method = $type == SDL_MOUSEMOTION    ? 'mousemove':
+                 $type == SDL_MOUSEBUTTONUP  ? 'mouseup':
+                 $type == SDL_MOUSEBUTTONDOWN? 'mousedown':
+                 return;
+
+    my $btn = $x >= $BTN_NEXT_X - 2? $self->btn_next:
+              return;
+
+    $btn->$method();
 }
 
 after render => sub {
--- lib/CamelDefense/Game/UI/Button.pm
+++ lib/CamelDefense/Game/UI/Button.pm
@@ -1,12 +1,31 @@
 package CamelDefense::Game::UI::Button;
 
 use Moose;
+use MooseX::Types::Moose qw(CodeRef);
 
 with 'CamelDefense::Role::Sprite';
 
-has icon => (is => 'ro', required => 1); # my image def
+has icon  => (is => 'ro', required => 1); # my image def
+has click => (is => 'ro', required => 1, isa => CodeRef);
 
 sub init_image_def { shift->icon }
+
+sub mousemove {
+    my $self = shift;
+}
+
+sub mousedown {
+    my $self = shift;
+    my ($x, $y) = @{ $self->xy };
+    $self->xy([$x, $y + 1]);
+}
+
+sub mouseup {
+    my $self = shift;
+    my ($x, $y) = @{ $self->xy };
+    $self->xy([$x, $y - 1]);
+    $self->click->();
+}
 
 1;
 
